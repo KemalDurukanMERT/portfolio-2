7386137fea652f57acdd6ec825502fac856ce28e
KemalDurukanMERT <kdurukanmert@gmail.com>
2023-02-02 18:47:16 +0100

    .

--- src/Components/HomeComponents/SayHi.jsx
+++ src/Components/HomeComponents/SayHi.jsx
@@ -14,12 +14,12 @@ const SayHi = () => {
         <div className="line border border-danger"></div>
         <div className="title text-dark mb-3">Full Stack Developer</div>
         <div className="buttons d-flex flex-wrap gap-1">
-          <Button variant="danger">
+          <Button variant="danger" className="button">
             <a href={resume} target="_blank" rel="noreferrer">
               Download Resume
             </a>
           </Button>
-          <Button variant="dark">
+          <Button variant="dark" className="button">
             <a href="#portfolio">View Portfolio</a>
           </Button>
         </div>
