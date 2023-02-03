80986e503a8bbff058bfaa97db769a9e560edb71
KemalDurukanMERT <kdurukanmert@gmail.com>
2023-02-03 01:17:28 +0100

    .

--- src/Components/HomeComponents/Resume.jsx
+++ src/Components/HomeComponents/Resume.jsx
@@ -1,5 +1,20 @@
 import React from "react";
 import logo from "../../Assets/ibm-logo-white.png";
+import bootstrap from "../../Assets/bootstrap.png";
+import css from "../../Assets/css.png";
+import git from "../../Assets/git.png";
+import github from "../../Assets/github.png";
+import html from "../../Assets/html.png";
+import javascript from "../../Assets/javascript.png";
+import linux from "../../Assets/linux.png";
+import postgresql from "../../Assets/postgresql.png";
+import python from "../../Assets/python.png";
+import react from "../../Assets/react.png";
+import redux from "../../Assets/redux.png";
+import sass from "../../Assets/sass.png";
+
+
+
 const Resume = () => {
   return (
     <div className="resume" id="resume">
@@ -112,15 +127,160 @@ const Resume = () => {
       </div>
       <div className="languages mt-5">
         <div className="line border border-danger"></div>
-        <div className="languages-header fs-4 text-secondary">Languages</div>
+        <div className="languages-header fs-4 text-secondary mb-2">
+          Languages
+        </div>
         <div className="language-area">
-          <div className="language-cart">
+          <div className="language-cart bg-warning rounded-2 p-1 m-1">
             Turkish
-            <span>
+            <span className="p-3 text-secondary">
               <i>Native</i>
             </span>
           </div>
-          <div className="language-cart">English<span><i>Professional Working Profeciency</i></span></div>
+          <div className="language-cart bg-warning rounded-2 p-1 m-1">
+            English
+            <span className="p-3 text-secondary">
+              <i>Professional Working Profeciency</i>
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="hard-skills">
+        <div className="line border border-danger"></div>
+        <div className="certificates-header fs-4 text-secondary">
+          Certificates
+        </div>
+        <div class="container-hard-skills">
+          <div class="container__progressbars">
+            <div class="progressbar">
+              <svg class="progressbar__svg">
+                <circle
+                  cx="80"
+                  cy="80"
+                  r="70"
+                  class="progressbar__svg-circle circle-html shadow-html"
+                >
+                  {" "}
+                </circle>
+              </svg>
+              <span class="progressbar__logo shadow-html"><img src={html} alt=""/></span>
+              <span class="progressbar__text shadow-html">HTML</span>
+            </div>
+
+            <div class="progressbar">
+              <svg class="progressbar__svg">
+                <circle
+                  cx="80"
+                  cy="80"
+                  r="70"
+                  class="progressbar__svg-circle circle-css shadow-css"
+                >
+                  {" "}
+                </circle>
+              </svg>
+              <span class="progressbar__logo shadow-css"><img src={css} alt=""/></span>
+              <span class="progressbar__text shadow-css">CSS</span>
+            </div>
+
+            <div class="progressbar">
+              <svg class="progressbar__svg">
+                <circle
+                  cx="80"
+                  cy="80"
+                  r="70"
+                  class="progressbar__svg-circle circle-scss shadow-scss"
+                >
+                  {" "}
+                </circle>
+              </svg>
+              <span class="progressbar__logo shadow-css"><img src={sass} alt="" /></span>
+              <span class="progressbar__text shadow-scss">SCSS</span>
+            </div>
+
+            <div class="progressbar">
+              <svg class="progressbar__svg">
+                <circle
+                  cx="80"
+                  cy="80"
+                  r="70"
+                  class="progressbar__svg-circle circle-js shadow-js"
+                >
+                  {" "}
+                </circle>
+              </svg>
+              <span class="progressbar__logo shadow-css"><img src={javascript} alt="" /></span>
+              <span class="progressbar__text shadow-js">JavaScript</span>
+            </div>
+
+            <div class="progressbar">
+              <svg class="progressbar__svg">
+                <circle
+                  cx="80"
+                  cy="80"
+                  r="70"
+                  class="progressbar__svg-circle circle-ts shadow-ts"
+                >
+                  {" "}
+                </circle>
+              </svg>
+              <span class="progressbar__text shadow-ts">TypeScript</span>
+            </div>
+
+            <div class="progressbar">
+              <svg class="progressbar__svg">
+                <circle
+                  cx="80"
+                  cy="80"
+                  r="70"
+                  class="progressbar__svg-circle circle-node shadow-node"
+                >
+                  {" "}
+                </circle>
+              </svg>
+              <span class="progressbar__text shadow-node">Node.js</span>
+            </div>
+
+            <div class="progressbar">
+              <svg class="progressbar__svg">
+                <circle
+                  cx="80"
+                  cy="80"
+                  r="70"
+                  class="progressbar__svg-circle circle-react shadow-react"
+                >
+                  {" "}
+                </circle>
+              </svg>
+              <span class="progressbar__logo shadow-css"><img src={react} alt="" /></span>
+              <span class="progressbar__text shadow-react">React.js</span>
+            </div>
+            <div class="progressbar">
+              <svg class="progressbar__svg">
+                <circle
+                  cx="80"
+                  cy="80"
+                  r="70"
+                  class="progressbar__svg-circle circle-angular shadow-angular"
+                >
+                  {" "}
+                </circle>
+              </svg>
+              <span class="progressbar__text shadow-angular">Angular.js</span>
+            </div>
+            <div class="progressbar">
+              <svg class="progressbar__svg">
+                <circle
+                  cx="80"
+                  cy="80"
+                  r="70"
+                  class="progressbar__svg-circle circle-vue shadow-vue"
+                >
+                  {" "}
+                </circle>
+              </svg>
+              <span class="progressbar__text shadow-vue">Vue.js</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
