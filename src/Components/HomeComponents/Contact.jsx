cddba81a1002f25d2c24a12286fe05023383222e
KemalDurukanMERT <kdurukanmert@gmail.com>
2023-02-05 22:37:45 +0100

    .

--- src/Components/HomeComponents/Contact.jsx
+++ src/Components/HomeComponents/Contact.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import MailForm from "./MaılForm";
+import Mail from "./Mail";
 
 function Contact() {
   const [focus, setFocus] = useState(false);
@@ -8,9 +8,13 @@ function Contact() {
       <div className="contact-header fs-1 fw-bold">Contact</div>
       <div className="line border border-danger"></div>
       <div className="subtitle text-secondary fs-4">Get in Touch</div>
-      <div className="mail-area" onFocus={() => setFocus(true)} onBlur={() => setFocus(false)}>
-        <div className="mail-form mt-3 w-75" >
-          <MailForm setFocus={setFocus}></MailForm>
+      <div className="mail-area d-flex" onFocus={() => setFocus(true)} onBlur={() => setFocus(false)}>
+        <div className="mail-form w-50 border border-danger" >
+          <Mail setFocus={setFocus}></Mail>
+        </div>
+        <div className="contact-symbol w-50 border border-danger">
+          <img src="" alt="" />
+          <div className="contact-logo"></div>
         </div>
         <div
           className="phone-side"
---
+++ src/Components/HomeComponents/Mail.jsx
@@ -0,0 +1,56 @@
+const Mail = ({setFocus}) => {
+  const handleSubmit = () => {
+    // e.preventDefault();
+    setTimeout(() => setFocus(false), 3000);
+  };
+  return (
+    <div className="container">
+      <h1>FormSubmit Demo</h1>
+      <form
+        target="_blank"
+        action="https://formsubmit.co/c26da42eacb77d5add0d2e7163d80e0e"
+        method="POST"
+        onSubmit={handleSubmit}
+      >
+        <div className="form-group">
+          <div className="form-row">
+            <div className="col">
+              <input
+                type="text"
+                name="name"
+                className="form-control"
+                placeholder="Full Name"
+                required
+              />
+            </div>
+            <div className="col">
+              <input
+                type="email"
+                name="email"
+                className="form-control"
+                placeholder="Email Address"
+                required
+              />
+            </div>
+          </div>
+        </div>
+        <div className="form-group">
+          <textarea
+            placeholder="Your Message"
+            className="form-control"
+            name="message"
+            rows={10}
+            required
+            defaultValue={""}
+          />
+        </div>
+        <button type="submit" className="btn btn-lg btn-dark btn-block">
+          Submit Form
+        </button>
+      </form>
+    </div>
+  );
+};
+
+
+export default Mail;
