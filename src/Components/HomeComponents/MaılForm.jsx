5b9ef37e3600f691df3463448e4c49bc00dec7f9
KemalDurukanMERT <kdurukanmert@gmail.com>
2023-02-05 08:18:11 +0100

    .

--- src/Components/HomeComponents/MaılForm.jsx
+++ src/Components/HomeComponents/MaılForm.jsx
@@ -7,7 +7,7 @@ const handleSubmit = (e) => {
   setTimeout(()=> setFocus(false),3000)
 }
   return (
-    <Form onSubmit={handleSubmit}>
+    <Form onSubmit={handleSubmit} >
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
         <Form.Control type="email" placeholder="Enter email" />
@@ -23,7 +23,7 @@ const handleSubmit = (e) => {
       <Form.Group className="mb-3" controlId="formBasicCheckbox">
         <Form.Check type="checkbox" label="Check me out" />
       </Form.Group>
-      <Button variant="primary" type="submit" >
+      <Button variant="primary" type="submit" onTouchEnd={handleSubmit} >
         Submit
       </Button>
     </Form>
